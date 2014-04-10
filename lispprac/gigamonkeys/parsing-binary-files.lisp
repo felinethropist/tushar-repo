49a03c444a4c5a94d33920f861a1c9fab6f572ec
Tushar Janefalkar <tusharj@codeaurora.org>
2014-04-10 11:31:11 -0700

    update to parsing binary files

--- lispprac/gigamonkeys/parsing-binary-files.lisp
+++ lispprac/gigamonkeys/parsing-binary-files.lisp
@@ -1,4 +1,7 @@
-(load "/home/tushar/Downloads/practicals-1.0.3/Chapter08/macro-utilities.lisp")
+
+;;change this path depending on location on each pc
+(load "~/Downloads/practicals-1.0.3/Chapter08/packages.lisp")
+(load "~/Downloads/practicals-1.0.3/Chapter08/macro-utilities.lisp")
 
 (defun read-u2 (in)
   (let ((u2 0))
@@ -62,8 +65,8 @@
 
 (in-package :cl-user)
 
-(defpackage :com.tj.binary-data
-  (:use :common-lisp :com.tj.macro-utilities)
+(defpackage :com.gigamonkeys.binary-data
+  (:use :common-lisp :com.gigamonkeys.macro-utilities)
   (:export :define-binary-class
 	   :define-tagged-binary-class
 	   :define-binary-type
@@ -81,10 +84,39 @@
   (let ((name (first spec)))
     `(,name :initarg ,(as-keyword name) :accessor ,name)))
 
-(defmacro define-binary-class (name slots)
-  `(defclass ,name()
-     ,(mapcar #'slot->defclass-slot slots)))
-
 (defgeneric read-value (type stream &key)
   (:documentation "Read a value of the given type from the stream"))
+
+(defun slot->read-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(setf ,name (read-value ',type ,stream ,@args))))
+
+(defun normalize-slot-spec (spec)
+  (list (first spec) (mklist (second spec))))
+
+(defun mklist (x)
+  (if (listp x)
+      x
+      (list x)))
+
+(defgeneric write-value (type stream value &key)
+  (:documentation "Write a value as the given type to a stream"))
+
+(defun slot->write-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+    `(write-value ',type ,stream ,name ,@args)))
+
+(defmacro define-binary-class (name slots)
+  (with-gensyms (typevar objectvar streamvar)
+    `(progn
+       (defclass ,name ()
+	 ,(mapcar #'slot->defclass-slot slots))
+       (defmethod read-value ((,typevar (eql ',name)) ,streamvar &key)
+	 (let ((,objectvar (make-instance ',name)))
+	   (with-slots ,(mapcar #'first slots) ,objectvar
+	     ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
+	   ,objectvar))
+       (defmethod write-value ((,typevar (eql ',name)) ,streamvar ,objectvar &key)
+	 (with-slots ,(mapcar #'first slots) ,objectvar
+	   ,@(mapcar #'(lambda (x) (slot->write-value x streamvar)) slots))))))
 
